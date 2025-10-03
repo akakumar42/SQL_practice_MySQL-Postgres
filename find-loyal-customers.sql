17b14807de36c55912623666cc5b24345b7fd922
Akash Kumar <akakumar.42berlin@gmail.com>
2025-10-03 18:22:12 +0200

    find loyal customers based on comsumers behaviour

---
+++ find-loyal-customers.sql
@@ -0,0 +1,12 @@
+SELECT 
+    customer_id
+FROM customer_transactions
+GROUP BY customer_id
+HAVING 
+    SUM(transaction_type = 'purchase') >= 3
+    AND DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30
+    AND (
+        SUM(transaction_type = 'refund') * 1.0 
+        / COUNT(*)  
+    ) < 0.2
+ORDER BY customer_id;
